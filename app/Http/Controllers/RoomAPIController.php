d557fa3ea3b46e70ec4d5e6cb0db6c685db6c42b
Oksana Bakhaieva <obahaeva@gmail.com>
2024-12-09 15:47:56 +0000

    got a successful response for a specific room, and given a correct error response message

---
+++ app/Http/Controllers/RoomAPIController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomAPIController extends Controller
+{
+   public function find(int $id){
+       $room = Room::find($id);
+       if($room){
+           return response()->json([
+               'message'=> 'Room successfully retrieved',
+               'data'=> [
+                   'id' => $room->id,
+                   'name' => $room->name,
+                   'rate' => $room->rate,
+                   'image' => $room->image,
+                   'min_capacity' => $room->min_capacity,
+                   'max_capacity' => $room->max_capacity,
+                   'description' => $room->description,
+                   'type' => [
+                       'id'=> $room->type->id,
+                       'name'=>$room->type->name,
+                    ]
+               ]
+           ]);
+       } else {
+           return response()->json([
+               'message'=> "Room with id {$id} not found"
+           ],404);
+       }
+   }
+}
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Room extends Model
 {
-    //
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
