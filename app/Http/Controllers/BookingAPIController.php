c2b933d0ddc8b2c1c4d6fd0df7a67ec83c563ac2
Oksana Bakhaieva <obahaeva@gmail.com>
2024-12-10 14:35:20 +0000

    checking the start  and the end dates for booking

--- app/Http/Controllers/BookingAPIController.php
+++ app/Http/Controllers/BookingAPIController.php
@@ -12,62 +12,55 @@ class BookingAPIController extends Controller
 {
     public function create(Request $request)
     {
+
         $request->validate([
             'room_id' => 'required|integer|exists:rooms,id',
             'customer' => 'required|string|max:255',
             'guests' => 'required|integer|min:1',
-            'start' => 'required|date|date_format:Y-m-d',
-            'end' => 'required|date|date_format:Y-m-d',
+            'start' => 'required|date|date_format:Y-m-d|after_or_equal:today',
+            'end' => 'required|date|date_format:Y-m-d|after:start',
         ]);
+
         $room = Room::find($request->room_id);
-        $bookingConflict = Booking::where('room_id', $room->id)
-            ->where('start', '>=', $request->start);
-
-
-
         $startDate = $request->start;
         $endDate = $request->end;
-            if ($room->min_capacity > $request->guests OR  $room->max_capacity < $request->guests) {
+        $bookingConflict = Booking::where('room_id', $room->id)
+            ->where(function ($query) use ($startDate, $endDate) {
+                $query->whereBetween('start', [$startDate, $endDate])
+                    ->orWhereBetween('end', [$startDate, $endDate])
+                    ->orWhere(function ($query) use ($startDate, $endDate) {
+                        $query->where('start', '<=', $startDate)
+                            ->where('end', '>=', $endDate);
+                    });
+            })
+            ->exists();
+
+        if ($room->min_capacity > $request->guests OR  $room->max_capacity < $request->guests) {
+            return response()->json([
+                'message' => "The {$room->name} room can only accommodate between {$room->min_capacity} and {$room->max_capacity} guests",
+            ],400);
+        }
+        elseif ($bookingConflict)
+        {
                 return response()->json([
-                    'message' => "The {$room->name} room can only accommodate between {$room->min_capacity} and {$room->max_capacity} guests",
-                ]);
-            }
-            elseif ($endDate < $startDate)
-            {
-                return response()->json([
-                   'message' => 'Start date must be before the end date'
+                    'message' => "The {$room->name} room is unavailable for the chosen dates."
                 ], 400);
-            }
-            elseif($bookingConflict->exists())
-            {
-                if()
-                {
+        }
+        else
+        {
+            $booking = new Booking();
 
-                }
-                return response()->json([
-                    'message' => "The {$room->name} room is unavailable for the chosen dates"
-                ], 400);
+            $booking->room_id = $request->room_id;
+            $booking->customer = $request->customer;
+            $booking->start = $request->start;
+            $booking->end = $request->end;
 
-//             check room_id -> check if the room is booked at any point
-//                 if the room has been booked -> check the start and end dates
-//                'booking conflict' => $bookingConflict->first()
+            $booking->save();
 
-            }
-            else
-            {
-                $booking = new Booking();
-
-                $booking->room_id = $request->room_id;
-                $booking->customer = $request->customer;
-                $booking->start = $request->start;
-                $booking->end = $request->end;
-
-                $booking->save();
-
-                return response()->json([
-                    'message' => 'Booking successfully created',
-                    'data' => $booking->room->min_capacity
-                    ], 201);
-            }
+            return response()->json([
+                'message' => "Booking successfully created",
+                'data' => $booking
+            ], 201);
+        }
     }
 }
