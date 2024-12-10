68cfdd7c1b466b02724c215bef3d6a85222f72bf
Oksana Bakhaieva <obahaeva@gmail.com>
2024-12-10 09:44:36 +0000

    updated tests

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -6,6 +6,7 @@ use App\Models\Room;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class RoomTest extends TestCase
@@ -16,7 +17,25 @@ class RoomTest extends TestCase
     {
         Room::factory()->create();
         $response = $this->get('/api/rooms/1');
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertJson(function(AssertableJson $json) {
+               $json->hasAll(['message', 'data'])
+                ->has('data', function(AssertableJson $json) {
+                    $json->hasAll(['id', 'name', 'rate', 'image',
+                        'min_capacity', 'max_capacity', 'description',
+                        'type'])
+                    ->whereAllType([
+                        'id' => 'integer',
+                        'name' => 'string',
+                        'rate' => 'integer',
+                        'image' => 'string',
+                        'min_capacity' => 'integer',
+                        'max_capacity' => 'integer',
+                        'description' => 'string',
+                        'type' => 'array'
+                    ]);
+                });
+            });
     }
     public function test_getRoom_by_id_failure(): void
     {
