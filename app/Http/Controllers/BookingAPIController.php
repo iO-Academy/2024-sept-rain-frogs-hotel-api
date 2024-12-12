fb12547d7f89840cfec593bd98126d07f72855bc
Oksana Bakhaieva <obahaeva@gmail.com>
2024-12-12 12:39:45 +0000

    updated filter on bookings to return error if room doesn't exist

--- app/Http/Controllers/BookingAPIController.php
+++ app/Http/Controllers/BookingAPIController.php
@@ -9,19 +9,17 @@ class BookingAPIController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Booking::query();
+        $request->validate([
+            'room_id' => 'nullable|exists:rooms,id',
+        ]);
 
+        $query = Booking::query();
         if ($request->has('room_id')) {
             $currentAndFutureBookings = $query->where('room_id', $request->room_id)
                 ->with('room:id,name')
                 ->where('end', '>', now())
                 ->get()?->makeHidden(['updated_at', 'room_id']);
-            if ($currentAndFutureBookings->isEmpty()) {
-                return response()->json([
-                    'message' => 'The selected room id is invalid.',
-                    'success' => false,
-                ],422);
-            }
+
             return response()->json([
                 'message' => 'Booking retrieved successfully.',
                 'success' => true,
